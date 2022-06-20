3e61c84b5955c3cf2fa41b876919e4c031dc528c
Gustavo Flores Romero <guxtavo825@gmail.com>
2022-06-20 15:51:57 -0500

    Actualización de inrerfaz y registro

---
+++ app/src/main/java/com/example/enterfiance/CreateAccountOptional.java
@@ -0,0 +1,14 @@
+package com.example.enterfiance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAccountOptional extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account_optional);
+    }
+}
