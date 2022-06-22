03722b2f8afe3e1d6185f984bf4ac0b82632631d
MarianaYaz <62482852+MarianaYaz@users.noreply.github.com>
2022-06-21 19:01:46 -0500

    investment 1

---
+++ app/src/main/java/com/example/enterfiance/Investment.java
@@ -0,0 +1,16 @@
+package com.example.enterfiance;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Investment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_investment);
+    }
+}
